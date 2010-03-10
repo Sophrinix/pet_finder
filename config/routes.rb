788dc46f615c3de39e03768570af735f79989d07
dschobel <dschobel@gmail.com>
2010-03-09 23:11:06 -0600

    added user session controller and some basic views to handle session creation/deletion

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ ActionController::Routing::Routes.draw do |map|
   map.resources :users
   map.connect ':controller/:action/:id'
   map.connect ':controller/:action/:id.:format'
+  map.login 'login', :controller => 'user_sessions', :action => 'new'  
+  map.logout 'logout', :controller => 'user_sessions', :action => 'destroy'  
+  map.resources :user_sessions 
+  map.root :controller => 'users'
 end
