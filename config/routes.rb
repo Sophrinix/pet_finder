e9f778459290adf71c017f0360cfc9ebaca52373
dschobel <dschobel@gmail.com>
2010-03-13 03:24:52 -0600

    converted all views to haml added missing helper method to application_controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ ActionController::Routing::Routes.draw do |map|
   map.resources :pets
   map.resources :users
   map.resources :user_sessions 
+  map.resources :admin
   map.login 'login', :controller => 'user_sessions', :action => 'new'  
   map.logout 'logout', :controller => 'user_sessions', :action => 'destroy'  
   map.root :controller => 'pets'
